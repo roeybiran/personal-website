b6d101f43c60e65b7267040d4c97e9c0e7a5c19f
roeybiran <roeybiran@icloud.com>
2024-03-31 15:47:49 +0300

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import remarkToc from "remark-toc";
 import remarkUnwrapImages from "remark-unwrap-images";
-import remarkFlexibleContainers from "remark-flexible-containers";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -9,7 +8,7 @@ export default defineConfig({
   site: "https://roeybiran.com",
   output: "hybrid",
   markdown: {
-    remarkPlugins: [remarkToc, remarkUnwrapImages, remarkFlexibleContainers],
+    remarkPlugins: [remarkToc, remarkUnwrapImages],
   },
   vite: {
     server: {
