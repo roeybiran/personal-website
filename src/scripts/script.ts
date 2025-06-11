0589a199fe0e6c91a7c2db07afb8f9db54374f45
Roey Biran <roeybiran@icloud.com>
2025-06-12 01:53:46 +0300

    wip

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,3 +1,5 @@
+import { gsap } from "gsap";
+
 document.querySelectorAll(".project-card__button").forEach((button) => {
   button.addEventListener("click", (e) => {
     const dialog = (e.target as HTMLButtonElement).dataset.popup;
@@ -17,3 +19,47 @@ document.querySelectorAll(".project-card__close-button").forEach((button) => {
 document.addEventListener("click", ({ target }) => {
   if (target instanceof HTMLDialogElement) target.close();
 });
+
+const tl = gsap.timeline({
+  paused: true,
+  defaults: {
+    ease: "power1.inOut",
+    duration: 0.3,
+  },
+});
+
+document
+  .querySelectorAll("[data-js-app-nav-mobile-menu-button]")
+  .forEach((button) => {
+    const menu = button.parentElement?.querySelector(".mobile-menu");
+    if (!menu || !(menu instanceof HTMLDivElement)) return;
+    const links = menu.querySelectorAll("a");
+
+    gsap.set(menu, {
+      scaleY: 0,
+      autoAlpha: 0,
+    });
+    gsap.set(links, {
+      opacity: 0,
+    });
+
+    tl.to(menu, {
+      scaleY: 1,
+      autoAlpha: 1,
+    }).to(links, {
+      opacity: 1,
+      stagger: 0.05,
+    });
+
+    button.addEventListener("click", () => {
+      const previousIsExpanded = menu.getAttribute("aria-expanded") === "true";
+      menu.setAttribute("aria-expanded", previousIsExpanded ? "false" : "true");
+      const newIsExpanded = !previousIsExpanded;
+
+      if (newIsExpanded) {
+        tl.play();
+      } else {
+        tl.reverse();
+      }
+    });
+  });
